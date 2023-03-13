c7ca718e09b3e2056342b34bdd8dbd03b11a1fa7
Milad Asghari Amlashi <miladasghari2016@gmail.com>
2023-03-13 14:57:34 +0330

    feat- add TaskCm

--- src/components/BoardCm.vue
+++ src/components/BoardCm.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import TaskCm from './TaskCm.vue'
+
 defineProps<{
   title: string
 }>()
@@ -9,10 +11,21 @@ const all = ref(3)
 </script>
 
 <template>
-  <v-card :title="title + `(${done}/${all})(${percent}%)`" class="mx-auto" width="400"> </v-card>
+  <v-card
+    :title="title + `(${done}/${all})(${percent}%)`"
+    class="d-flex align-center flex-column"
+    width="400"
+  >
+    <TaskCm
+      title="Task"
+      attachments="attachments"
+      estimate-time="estimate-time"
+      description="description"
+    />
+  </v-card>
 </template>
 <style scoped>
-:deep(.v-card-title) {
+:deep(.v-card) {
   text-align: center;
 }
 </style>
---
+++ src/components/TaskCm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps<{
+  title: string
+  description: string
+  estimateTime: string
+  attachments: string
+}>()
+</script>
+
+<template>
+  <v-card class="my-2" width="380">
+    <p class="text-h6">title:{{ title }}</p>
+    <p class="text-h6">description:{{ description }}</p>
+    <p class="text-h6">estimateTime:{{ estimateTime }}</p>
+    <p class="text-h6">attachments:{{ attachments }}</p>
+  </v-card>
+</template>
+<style scoped></style>
